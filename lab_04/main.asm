15cfc8752c2f8db6f742bd808d4d8f67da855f3b
theGreatCanyon <example@example.com>
2026-03-30 00:30:56 +0300

    writen matrix read

--- lab_04/main.asm
+++ lab_04/main.asm
@@ -1,5 +1,6 @@
 MAX_HEIGHT EQU 9
 MAX_WIDTH  EQU 9
+ZERO_CODE EQU '0'
 
 section .data
     matrix db MAX_HEIGHT * MAX_WIDTH dup(0)
@@ -11,83 +12,78 @@ section .data
     msg2 db "Enter matrix line by line (each line should have <width> number of digits with no spaces):", 10
     MSG_2_LEN EQU $ - msg2
 
+
 section .text
     global _start
 
 _start:
-    ; print first message
     mov rax, 1
     mov rdi, 1
     mov rsi, msg1
     mov rdx, MSG_1_LEN
     syscall
 
-    ; read height line (digit + newline)
     mov rax, 0
     mov rdi, 0
     mov rsi, read_buffer
-    mov rdx, 2
+    mov rdx, 2                                  ; Read height and '/n'.
     syscall
     mov al, [read_buffer]
-    sub al, '0'
+    sub al, ZERO_CODE
     mov [height], al
 
-    ; read width line (digit + newline)
     mov rax, 0
     mov rdi, 0
     mov rsi, read_buffer
-    mov rdx, 2
+    mov rdx, 2                                  ; Read width and '/n'.
     syscall
     mov al, [read_buffer]
-    sub al, '0'
+    sub al, ZERO_CODE
     mov [width], al
 
-    ; print second message
     mov rax, 1
     mov rdi, 1
     mov rsi, msg2
     mov rdx, MSG_2_LEN
     syscall
 
-    mov rcx, 0                    ; row = 0
-
+    mov r12, 0                                  ; row index = 0.
 row_loop:
     movzx r8, byte [height]
-    cmp rcx, r8
-    jge done
+    cmp r12, r8
+    jge exit_row_loop
 
-    ; read one matrix row: width digits + newline
-    movzx rdx, byte [width]
-    inc rdx
     mov rax, 0
     mov rdi, 0
     mov rsi, read_buffer
+    movzx rdx, byte [width]
+    inc rdx                                   ; Read width + '/n'.
     syscall
 
-    ; destination = matrix + row * MAX_WIDTH
-    mov rax, rcx
-    imul rax, MAX_WIDTH
-    lea rdi, [matrix + rax]
-
-    xor rbx, rbx                    ; col = 0
-
+    ; Destination pointer for current row:
+    ; rdi = &matrix[row * MAX_WIDTH]
+    mov rax, r12                              ; rax = row.
+    imul rax, MAX_WIDTH                       ; rax = row * 9.
+    lea rdi, [matrix + rax]                   ; Row base address.
+    mov r13, 0                                ; col index = 0.
 col_loop:
     movzx r8, byte [width]
-    cmp rbx, r8
-    jge next_row
+    cmp r13, r8
+    jge exit_col_loop
 
-    mov al, [read_buffer + rbx]
-    sub al, '0'
-    mov [rdi + rbx], al
+    mov al, [read_buffer + r13]               ; Read digit from input row.
+    sub al, ZERO_CODE                         ; Convert code to numeric value.
+    mov [rdi + r13], al                       ; Store into matrix[row][col].
 
-    inc rbx
-    jmp col_loop
+    inc r13                                   ; col++.
+    jmp col_loop                              ; Continue processing row.
 
-next_row:
-    inc rcx
-    jmp row_loop
+exit_col_loop:
+    inc r12                                   ; row++.
+    jmp row_loop                              ; Continue processing matrix.
 
-done:
-    mov rax, 60
-    xor rdi, rdi
+exit_row_loop:
+    mov rax, 60                               ; Syscall: exit.
+    xor rdi, rdi                              ; Exit status = 0.
     syscall
+
