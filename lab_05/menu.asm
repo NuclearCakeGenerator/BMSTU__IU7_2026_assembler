62eeb24847d3fa260bedf2a63981362802dfe0e0
Mr Tinker <example@example.com>
2026-04-14 23:24:02 +0300

    menu implemented

--- lab_05/menu.asm
+++ lab_05/menu.asm
@@ -4,15 +4,18 @@
 
 extern system
 
+global get_option
+
 BUFFER_SIZE equ 1
 
 section .data
-    menu_msg db "[MENU]", CHAR_NEWLINE
-    db "[1]", CHAR_TAB, "Enter a new 16 bit SIGNED NEXADECIMAL number.", CHAR_NEWLINE
-    db "[2]", CHAR_TAB, "Print a number in UNSIGNED BINARY form.", CHAR_NEWLINE
-    db "[3]", CHAR_TAB, "Print a 8bit clipped number in SIGNED OCTAL form.", CHAR_NEWLINE
-    db "[4]", CHAR_TAB, "Print a LEAST two's degree EXCEEDING this UNSIGNED number.", CHAR_NEWLINE
-    db CHAR_NEWLINE, "Press a number key with a proper menu option", CHAR_NEWLINE
+    menu_msg db "[MENU]",  CHAR_CARRIAGE_RETURN, CHAR_LINE_FEED
+    db "[1]", CHAR_TAB, "Enter a new 16 bit SIGNED NEXADECIMAL number.", CHAR_CARRIAGE_RETURN, CHAR_LINE_FEED
+    db "[2]", CHAR_TAB, "Print a number in UNSIGNED BINARY form.",  CHAR_CARRIAGE_RETURN, CHAR_LINE_FEED
+    db "[3]", CHAR_TAB, "Print a 8bit clipped number in SIGNED OCTAL form.",  CHAR_CARRIAGE_RETURN, CHAR_LINE_FEED
+    db "[4]", CHAR_TAB, "Print a LEAST two's degree EXCEEDING this UNSIGNED number.",  CHAR_CARRIAGE_RETURN, CHAR_LINE_FEED
+    db "[5]", CHAR_TAB, "Exit.",  CHAR_CARRIAGE_RETURN, CHAR_LINE_FEED
+    db  CHAR_CARRIAGE_RETURN, CHAR_LINE_FEED, "Press a number key with a proper menu option",  CHAR_CARRIAGE_RETURN, CHAR_LINE_FEED
     MENU_MSG_LEN equ $ - menu_msg
     raw_cmd db "stty raw -echo",0
     sane_cmd db "stty sane",0
@@ -27,50 +30,31 @@ get_option:
                                                     ;     system(raw_cmd) via rdi
     mov rdi, raw_cmd
     call system
+                                                    ;     fprintf(menu_msg)
+    print_string menu_msg, MENU_MSG_LEN
                                                     ;     while(true)
 get_option_loop:
                                                     ;     {
-                                                    ;         fprintf(menu_msg)
-    mov rax, SYS_WRITE
-    mov rdi, STDOUT
-    mov rsi, menu_msg
-    mov rdx, MENU_MSG_LEN
-    syscall
-                                                    ;         ch = fgetc() // finally store in rbx
-    mov rax, SYS_READ
-    mod rdi, STDIN
-    mov rsi, buf
-    mov rdx, BUFFER_SIZE
-                                                    ;         if (ch == '1')
-                                                    ;             break
+                                                    ;         ch = fgetc()
+    read_string buf, BUFFER_SIZE
+                                                    ;         if ('1' > ch)
+                                                    ;          continue
     mov cl, '1'
     cmp cl, [buf]
-    je end_get_option_loop
-                                                    ;         else if (ch == '2')
-                                                    ;             break
-    mov cl, '2'
+    jg get_option_loop
+                                                    ;         if ('5' < ch)
+                                                    ;             continue
+    mov cl, '5'
     cmp cl, [buf]
-    je end_get_option_loop
-                                                    ;         else if (ch == '3')
-                                                    ;             break
-    mov cl, '3'
-    cmp cl, [buf]
-    je end_get_option_loop
-                                                    ;         else if (ch == '4')
-                                                    ;             break
-    mov cl, '4'
-    cmp cl, [buf]
-    je end_get_option_loop
+    jl get_option_loop
+                                                    ;         break
                                                     ;     }
-jmp get_opton_loop
 end_get_option_loop:
                                                     ;     system(sane_cmd) via rdi
-    mov rdi, raw_cmd
+    mov rdi, sane_cmd
     call system    
                                                     ;     return ch // via rax
-    movzx rax [buf]
+    movzx rax, byte [buf]
     sub rax, '0'
                                                     ; }
 end_get_option:
-
-
