60bf9ab8048256d20cf3c40a756463907d93f78d
Mr Tinker <example@example.com>
2026-04-15 22:52:39 +0300

    updated read

--- lab_05/number_read.asm
+++ lab_05/number_read.asm
@@ -24,66 +24,73 @@ enter_number:
 
 hex_to_bin:
 ; string is at [number]
-mov al, '-'
-mov ah, [number]
-cmp al, ah
-jne check_as_positive
-mov rsi, 1   ; for sign flag
-dec rcx      ; skip the '-' character
-jmp check_length
+    mov al, '-'
+    mov ah, [number]
+    cmp al, ah
+    jne check_as_positive
+    mov rsi, 1   ; for sign flag
+    dec rcx      ; skip the '-' character
+    jmp check_length
 check_as_positive:
-mov rsi, 0
+    mov rsi, 0
 
 check_length:
     cmp rcx, 4
     jg invalid_digit
+    push r12
+    mov r12, rcx ; save number length
 
 convert_abs:
-xor rax, rax ; for the resulting number
+    xor rax, rax ; for the resulting number
 fetch_digit:
-xor rdx, rdx  ; for fetched character
-mov dl, [number + rcx - 1 + rsi]
+    xor rdx, rdx  ; for fetched character
+    mov rdi, rsi
+    add rdi, r12
+    sub rdi, rcx ; calculate the address of the current digit
 
-cmp dl, 0
-je end_fetch_digits
+    mov dl, [number + rdi]
+
+    cmp dl, 0
+    je end_fetch_digits
 ;convert_digit
-cmp dl, '0'
-jl invalid_digit
-cmp dl, '9'
-jg check_uppercase
-sub dl, '0'
-jmp append_digit
+    cmp dl, '0'
+    jl invalid_digit
+    cmp dl, '9'
+    jg check_uppercase
+    sub dl, '0'
+    jmp append_digit
 
 check_uppercase:
-cmp dl, 'A'
-jl invalid_digit
-cmp dl, 'F'
-jg check_lowercase
-sub dl, 'A' - 10
-jmp append_digit
+    cmp dl, 'A'
+    jl invalid_digit
+    cmp dl, 'F'
+    jg check_lowercase
+    sub dl, 'A' - 10
+    jmp append_digit
 
 check_lowercase:
-cmp dl, 'a'
-jl invalid_digit
-cmp dl, 'f'
-jg invalid_digit
-sub dl, 'a' - 10
-jmp append_digit
+    cmp dl, 'a'
+    jl invalid_digit
+    cmp dl, 'f'
+    jg invalid_digit
+    sub dl, 'a' - 10
+    jmp append_digit
 
 append_digit:
     shl rax, 4
     or rax, rdx
 
-loop fetch_digit
+    loop fetch_digit
 end_fetch_digits:
 ; rax now contains the absolute value of the number
 ; rsi indicates if the number is negative (1) or positive (0)
 ; We can now convert it to two's complement if it's negative
-cmp rsi, 0
-je end_conversion
+    cmp rsi, 0
+    je end_conversion
     neg rax
 end_conversion:
     mov rbx, rax
+    pop r12
     ret
 
 invalid_digit:
