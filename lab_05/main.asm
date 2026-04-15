af28b09d365fb06ec2ca4669e2cb773f88c9f75b
Mr Tinker <example@example.com>
2026-04-15 17:02:18 +0300

    done commands handler array

--- lab_05/main.asm
+++ lab_05/main.asm
@@ -3,32 +3,30 @@
 %include "../linux_sys.inc"
 
 extern get_option
-; extern enter_number
-; extern print_ub_number
-; extern print_so_number
-; extern print_exceeding_degree
+extern enter_number
+extern print_ub_number
+extern print_so_number
+extern print_exceeding_degree
 
 global main
 
 section .data
-    ; handlers dq enter_number, print_ub_number, print_so_number, print_exceeding_degree, end_main
+    handlers dq 0 enter_number, print_ub_number, print_so_number, print_exceeding_degree, end_main
+    buf db 0, CHAR_CARRIAGE_RETURN, CHAR_LINE_FEED
 
 section .bss
-    buf resb 1
     number resw 1
 
 section .text
-                                                    ; int main(void)
 main:
-                                                    ; {
-                                                    ;     option = get_option() // in rax
+    call enter_number
+
+event_loop:
     call get_option
-                                                    ;     fprintf(option)
-    add al, '0'
-    mov [buf], al
-    print_string buf, 1
-                                                    ;     return 0
-    exit 0
-                                                    ; }
+    cmp rax, 5
+    je end_main
+
+    call qword [handlers + rax*8]
+    jmp event_loop
 end_main:
     exit 0
